7b12f5fedc7b7973470e62919a15335908a1b157
nyxia91 <danielchristian0711@Gmail.com>
2019-03-18 01:39:59 +0700

    Main Menu

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
